a7bda2515cfb426925c3f173cf1a25f215e7dd86
Kingsley <kifeanyi216@gmail.com>
2025-05-28 15:31:11 +0100

    Working with Enums & Finishing Table Creation II

--- 02-create-users-table.sql
+++ 02-create-users-table.sql
@@ -1,4 +1,6 @@
+CREATE TYPE employment_status AS ENUM('employed', 'self_employed', 'unemployed');
 CREATE TABLE users (
     full_name VARCHAR(200),
     yearly_salary INT,
+    current_status employment_status -- current_status ENUM('employed', 'self_employed', 'unemployed') -- MySQL syntax
 );
