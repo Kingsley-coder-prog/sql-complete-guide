1c09b0d8c72947221876161045772ba83de5d605
Kingsley <kifeanyi216@gmail.com>
2025-05-28 15:54:35 +0100

    Inserting & Querying More Data

--- 03-insert-users-data.sql
+++ 03-insert-users-data.sql
@@ -1,2 +1,6 @@
+-- INSERT INTO users (yearly_salary, full_name, current_status)
+-- VALUES(19000, 'King Mike', 'self_employed');
 INSERT INTO users (yearly_salary, full_name, current_status)
-VALUES(19000, 'King Mike', 'self_employed');
+VALUES(25000, 'Julie Barnes', 'employed');
+INSERT INTO users (yearly_salary, full_name, current_status)
+VALUES(0, 'Michael Smith', 'unemployed');
