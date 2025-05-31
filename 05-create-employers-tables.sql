066273ceba8bae0d2ab43904f0de09f29f6d5e2f
Kingsley <kifeanyi216@gmail.com>
2025-05-31 20:35:36 +0100

    Inserting & Querying More Data III

--- 05-create-employers-tables.sql
+++ 05-create-employers-tables.sql
@@ -2,5 +2,5 @@ CREATE TABLE employers (
     company_name VARCHAR(250),
     company_address VARCHAR(300),
     -- yearly_revenue FLOAT(5,2) -- Approxiamtion, Allowed: 123.12, 12.1, Not allowed: 1234.12, 1234.123
-    yearly_revenue NUMERIC(5, 2) -- Exact value with 5 digits in total and 2 digits after the decimal point
+    yearly_revenue NUMERIC(5, 2) -- Exact value
 );
