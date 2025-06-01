ea1c52e2aabd27acc6eec0cc7d666cdc6c73ceaf
Kingsley <kifeanyi216@gmail.com>
2025-06-01 19:51:32 +0100

    create employees table

--- 05-create-employers-tables.sql
+++ 05-create-employers-tables.sql
@@ -2,5 +2,6 @@ CREATE TABLE employers (
     company_name VARCHAR(250),
     company_address VARCHAR(300),
     -- yearly_revenue FLOAT(5,2) -- Approxiamtion, Allowed: 123.12, 12.1, Not allowed: 1234.12, 1234.123
-    yearly_revenue NUMERIC(5, 2) -- Exact value
+    yearly_revenue NUMERIC(5, 2) -- Exact value with 5 digits total and 2 after the decimal
+    -- company_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
