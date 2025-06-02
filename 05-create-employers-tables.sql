c953436a586b475f78053b441f2efe7b02699453
Kingsley <kifeanyi216@gmail.com>
2025-06-02 03:43:16 +0100

    Adding Boolean Value Types

--- 05-create-employers-tables.sql
+++ 05-create-employers-tables.sql
@@ -2,6 +2,7 @@ CREATE TABLE employers (
     company_name VARCHAR(250),
     company_address VARCHAR(300),
     -- yearly_revenue FLOAT(5,2) -- Approxiamtion, Allowed: 123.12, 12.1, Not allowed: 1234.12, 1234.123
-    yearly_revenue NUMERIC(5, 2) -- Exact value with 5 digits total and 2 after the decimal
-    -- company_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+    yearly_revenue NUMERIC(5, 2),
+    -- Exact value 
+    is_hiring BOOLEAN
 );
