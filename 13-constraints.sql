1a09ac663bab463028af171b7caece710a0e1516
Kingsley <kifeanyi216@gmail.com>
2025-06-03 04:59:43 +0100

    Exploring the NOT NULL Constraint II

--- 13-constraints.sql
+++ 13-constraints.sql
@@ -5,4 +5,4 @@
 ALTER TABLE users -- ALTER COLUMN full_name SET NOT NULL, -- PostgreSQL syntax
     -- ALTER COLUMN current_status SET NOT NULL; -- PostgreSQL syntax
 MODIFY COLUMN full_name VARCHAR(300) NOT NULL,
-    MODIFY COLUMN current_status ENUM('employed', 'self-employed', 'unemployed') NOT NULL;
+    MODIFY COLUMN current_status ENUM('employed', 'self_employed', 'unemployed') NOT NULL;
